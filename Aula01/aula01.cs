a8caf2a2ea6a13e62aec8c00175b6f634c60da7c
unknown <carlos.viniciusufrpe@gmail.com>
2022-04-19 10:10:12 -0300

    primeiro programinha c#

---
+++ Aula01/aula01.cs
@@ -0,0 +1,6 @@
+using System; //indicando que quero utilizar a biblioteca system que possui os comandos básicos de entrada e saída.
+class Principal{
+    static void Main(){
+        Console.Write("Olá Mundo...");
+    }
+}
