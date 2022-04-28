0dedca1616f7b9ccea50c52fb8d80837a5229305
Carlos Vinicius Martins da Silva <carlos.vinicius@detran.pe.gov.br>
2022-04-28 15:48:34 -0300

    operadores e operações

---
+++ Aula05/Aula05.cs
@@ -0,0 +1,8 @@
+using System;
+class Aula05{
+    static void Main(){
+
+        int res = (10 + 5)*2;
+        Console.WriteLine(res);
+    }
+}
---
+++ Aula05/Aula05_1.cs
@@ -0,0 +1,15 @@
+using System;
+class Aula05{
+    static void Main(){
+
+        bool res=10<5;
+        Console.WriteLine(res);
+
+        bool res2 = (5<7)|(10>11);
+        Console.WriteLine(res2);
+
+        bool res3 = (5>7)&(10>8);
+        Console.WriteLine(res3);
+
+    }
+}
