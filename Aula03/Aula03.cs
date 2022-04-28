2ba613d2ec6f18c81f2abd83cdbf29d5521f806a
Carlos Vinicius Martins da Silva <carlos.vinicius@detran.pe.gov.br>
2022-04-28 14:23:29 -0300

    trbalhando com variáveis

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,14 @@
+using System;
+class Aula03{
+    static void Main(){
+        byte n1 = 10; //variável de 8 bits
+        int num = 0; //inteiro de 32 bits
+        char letra = 'c'; //um caracter apenas, usa apóstrofo ao invés de aspas.
+        float valor = 5.3f;
+        string nome = "carlos";
+
+        var aux = 10; // var não declara tipo da variável.
+
+        Console.WriteLine("valor da variavel: "+aux);
+    }
+}
---
+++ Aula03/Aula03_1.cs
@@ -0,0 +1,13 @@
+using System;
+class Aula03{
+    static void Main(){
+        int num1, num2, res;
+
+        num1=10;
+        num2=2;
+        res=num1*num2;
+
+
+        Console.WriteLine("A multiplicação de "+num1+" e "+num2+" é igual a: "+res);
+    }
+}
