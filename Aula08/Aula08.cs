927066f9dd11955b428a886f58a9032fc5d8b942
unknown <carlos.viniciusufrpe@gmail.com>
2022-04-30 17:01:12 -0300

    lendo valores do teclado

---
+++ Aula08/Aula08.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula08{
+    static void Main(){
+        
+        string nome;
+
+        Console.Write("Digite seu nome: ");
+        nome=Console.ReadLine();
+        Console.WriteLine("Nome digitado: {0}", nome);
+    }
+}
---
+++ Aula08/Aula08_1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula08{
+    static void Main(){
+        int v1,v2,soma;
+        
+
+        Console.Write("Digite o primeiro valor: ");
+        v1=int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        v2=int.Parse(Console.ReadLine());
+        soma = v1+v2;
+        Console.WriteLine("A soma de {0} mais {1} é igual a {2}", v1,v2,soma);
+    }
+}
