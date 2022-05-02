7562a29e455ff38f9a5bc9538b16ece4b2907c7e
unknown <carlos.viniciusufrpe@gmail.com>
2022-05-01 22:22:45 -0300

    Conversões de tipos usando typecast

---
+++ Typecast/Aula11.cs
@@ -0,0 +1,9 @@
+using System;
+class Aula11{
+    static void Main(){
+        float n1 = 10.5f;
+        int n2=(int)n1;  //operação de cast. Neste caso, typecast.
+
+        Console.WriteLine(n2);
+    }
+}
---
+++ Typecast/Aula11_1.cs
@@ -0,0 +1,9 @@
+using System;
+class Aula11{
+    static void Main(){
+        int vInt = 10;
+        short vShort = (short)vInt;
+
+        Console.WriteLine(vShort);
+    }
+}
