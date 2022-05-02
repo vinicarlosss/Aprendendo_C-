334ad4e6e507810d0110612e5ccd97fb6be1f8ea
unknown <carlos.viniciusufrpe@gmail.com>
2022-05-01 22:12:45 -0300

    utilizando enumeradores

---
+++ Enumeradores/Aula10.cs
@@ -0,0 +1,11 @@
+using System;
+class Aula10{
+
+    enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+    static void Main(){
+
+        DiasSemana ds = DiasSemana.Domingo;
+
+        Console.WriteLine(ds);
+    }
+}
