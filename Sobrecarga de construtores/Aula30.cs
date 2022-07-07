11fa7f2d20a36102e7cd6475da488efd368e68a9
Carlos Vinicius Martins da Silva <carlos.vinicius@detran.pe.gov.br>
2022-07-07 15:10:09 -0300

    sobrecarga de construtores

---
+++ Sobrecarga de construtores/Aula30.cs
@@ -0,0 +1,52 @@
+using System;
+
+public class Jogador{
+    public int energia;
+    public bool vivo;
+
+    public string nome;
+    public Jogador(){
+        energia = 100;
+        vivo = true;
+        nome="Jogador";
+    }
+    public Jogador(string n){
+        energia=100;
+        vivo=true;
+        nome = n;
+    }
+        public Jogador(string n, int e){
+        energia=e;
+        vivo=true;
+        nome = n;
+    }
+        public Jogador(string n, int e, bool v){
+        energia=e;
+        vivo=v;
+        nome = n;
+    }
+
+    public void info(){
+        Console.WriteLine("Nome jogador:{0}", nome);
+        Console.WriteLine("Energia jogador:{0}", energia);
+        Console.WriteLine("Estado jogador:{0}", vivo);
+
+    }
+
+}
+class Aula30{
+    static void Main(){
+
+        Jogador j1=new Jogador();
+        Jogador j2=new Jogador("Carlos");
+        Jogador j3=new Jogador("Matheus",100);
+        Jogador j4=new Jogador("Zigofreudo",30, true);
+        Jogador j5=new Jogador("Benegundesfirno",0, false);
+
+        j1.info();
+        j2.info();
+        j3.info();
+        j4.info();
+        j5.info();
+    }
+}
