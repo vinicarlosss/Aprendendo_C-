1785ff600479ef452b8a752f6b262ae7f853dbf2
vinicarlosss <carlos.viniciusufrpe@gmail.com>
2022-07-08 16:11:41 -0300

    Metodos Virtuais

---
+++ Metodos Virtuais/Aula38.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Base{
+    public Base(){
+        Console.WriteLine("Contrutor da clasee Base");
+    }
+
+    virtual public void info(){
+        Console.WriteLine("Base");
+    }
+}
+
+class Derivada1:Base{
+    public Derivada1(){
+        Console.WriteLine("Contrutor da clasee Derivada 1");
+    }
+
+    override public void info(){
+        Console.WriteLine("Derivada 1");
+    }
+}
+
+class Derivada2:Derivada1{
+    public Derivada2(){
+        Console.WriteLine("Contrutor da clasee Derivada 2");
+    }
+    override public void info(){
+        Console.WriteLine("Derivada 2");
+    }
+}
+
+class Aula38{
+    static void Main(){
+       Base Ref;
+       Derivada1 derivada1 = new Derivada1();
+       Derivada2 derivada2 = new Derivada2(); 
+       Ref = derivada1;
+
+       Ref.info();
+    }
+}
