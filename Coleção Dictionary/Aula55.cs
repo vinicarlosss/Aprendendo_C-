0bb624a3d1ec9f9b5ebf313e495423da135f3f4a
vinicarlosss <carlos.viniciusufrpe@gmail.com>
2022-07-15 15:21:41 -0300

    Coleção Dictionary

---
+++ Coleção Dictionary/Aula55.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+class Aula55{
+    static void Main(){
+        Dictionary <int, string> veiculos = new Dictionary<int, string>();
+
+        veiculos.Add(10, "Carro");
+        veiculos.Add(5, "Avião");
+        veiculos.Add(0, "Navio");
+        veiculos.Add(20, "Moto");
+        veiculos.Add(15, "Patinete");
+
+        //veiculos.Clear();
+        veiculos.Remove(20);
+        Console.WriteLine("Tamanho do Dictionary: {0}", veiculos.Count);
+        int chave = 20;
+        if(veiculos.ContainsKey(chave)){
+            Console.WriteLine("A chave {0} está na coleção", chave);
+            
+        }else{
+            Console.WriteLine("A chave {0} não está na coleção", chave);
+        }
+        string valor = "Navio";
+        if(veiculos.ContainsValue(valor)){
+            Console.WriteLine("O valor {0} está na coleção", valor);
+            
+        }else{
+            Console.WriteLine("O valor {0} não está na coleção", valor);
+        }
+
+        foreach(KeyValuePair <int, string> v in veiculos){
+            Console.WriteLine(v.Value);
+        }
+
+    }
+}
